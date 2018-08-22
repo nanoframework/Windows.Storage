a593c8f7fd5a6635c370683d2614e6949ba1e89a
Robin Jones <robin@networkfusion.co.uk>
2018-08-22 11:34:09 +0100

    methods for pathio

--- source/PathIO.cs
+++ source/PathIO.cs
@@ -12,5 +12,50 @@ namespace Windows.Storage
     /// </summary>
     public static class PathIO
     {
+        public static IAsyncAction AppendLinesAsync(String absolutePath, IEnumerable<String> lines)
+        { }
+
+        public static IAsyncAction AppendLinesAsync(String absolutePath, IEnumerable<String> lines, UnicodeEncoding encoding)
+        { }
+
+        public static IAsyncAction AppendTextAsync(String absolutePath, String contents)
+        { }
+
+        public static IAsyncAction AppendTextAsync(String absolutePath, String contents, UnicodeEncoding encoding)
+        { }
+
+        public static IAsyncOperation<IBuffer> ReadBufferAsync(String absolutePath)
+        { }
+
+        public static IAsyncOperation<IList<string>> ReadLinesAsync(String absolutePath)
+        { }
+
+        public static IAsyncOperation<IList<string>> ReadLinesAsync(String absolutePath, UnicodeEncoding encoding)
+        { }
+
+        public static IAsyncOperation<string> ReadTextAsync(String absolutePath)
+        { }
+
+        public static IAsyncOperation<string> ReadTextAsync(String absolutePath, UnicodeEncoding encoding)
+        { }
+
+        public static IAsyncAction WriteBufferAsync(String absolutePath, IBuffer buffer)
+        { }
+
+        public static IAsyncAction WriteBytesAsync(String absolutePath, Byte[] buffer)
+        { }
+
+        public static IAsyncAction WriteLinesAsync(String absolutePath, IEnumerable<String> lines)
+        { }
+
+        public static IAsyncAction WriteLinesAsync(String absolutePath, IEnumerable<String> lines, UnicodeEncoding encoding)
+        { }
+
+        public static IAsyncAction WriteTextAsync(String absolutePath, String contents)
+        { }
+
+        public static IAsyncAction WriteTextAsync(String absolutePath, String contents, UnicodeEncoding encoding)
+        { }
+
     }
 }
