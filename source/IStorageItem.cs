72516757d50fd59477d48b8665b4db1ce22b0496
Robin Jones <robin@networkfusion.co.uk>
2018-09-18 15:02:24 +0100

    uncommented some required classes and associated properties updated nugets

--- source/IStorageItem.cs
+++ source/IStorageItem.cs
@@ -1,36 +1,42 @@
-﻿////
-//// Copyright (c) 2018 The nanoFramework project contributors
-//// See LICENSE file in the project root for full license information.
-////
+﻿//
+// Copyright (c) 2018 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
 
-//using System;
+using System;
 
-//namespace Windows.Storage
-//{
-//    /// <summary>
-//    /// Manipulates storage items (files and folders) and their contents, and provides information about them.
-//    /// </summary>
-//    public interface IStorageItem
-//    {
-//        public FileAttributes Attributes { get; }
+namespace Windows.Storage
+{
+    /// <summary>
+    /// Manipulates storage items (files and folders) and their contents, and provides information about them.
+    /// </summary>
+    public interface IStorageItem
+    {
+        //        public FileAttributes Attributes { get; }
 
-//        public DateTimeOffset DateCreated { get; }
+        //        public DateTimeOffset DateCreated { get; }
 
-//        public string Name { get; }
+        /// <summary>
+        /// Gets the name of the item including the file name extension if there is one.
+        /// </summary>
+        string Name { get; }
 
-//        public string Path { get; }
+        /// <summary>
+        /// Gets the full file-system path of the item, if the item has a path.
+        /// </summary>
+        string Path { get; }
 
-//        public IAsyncAction DeleteAsync();
+        //        public IAsyncAction DeleteAsync();
 
-//        public IAsyncAction DeleteAsync(StorageDeleteOption option);
+        //        public IAsyncAction DeleteAsync(StorageDeleteOption option);
 
-//        public IAsyncOperation<BasicProperties> GetBasicPropertiesAsync();
+        //        public IAsyncOperation<BasicProperties> GetBasicPropertiesAsync();
 
-//        public bool IsOfType(StorageItemTypes type);
+        //        public bool IsOfType(StorageItemTypes type);
 
-//        public IAsyncAction RenameAsync(String desiredName);
+        //        public IAsyncAction RenameAsync(String desiredName);
 
-//        public IAsyncAction RenameAsync(String desiredName, NameCollisionOption option);
+        //        public IAsyncAction RenameAsync(String desiredName, NameCollisionOption option);
 
-//    }
-//}
+    }
+}
--- source/KnownFolders.cs
+++ source/KnownFolders.cs
@@ -1,16 +1,39 @@
-﻿////
-//// Copyright (c) 2018 The nanoFramework project contributors
-//// See LICENSE file in the project root for full license information.
-////
+﻿//
+// Copyright (c) 2018 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
 
-//using System;
+using System;
 
-//namespace Windows.Storage
-//{
-//    /// <summary>
-//    /// Provides access to common locations that contain user content. This includes content from a user's local libraries (such as Documents, Pictures, Music, and Videos), HomeGroup, removable devices, and media server devices.
-//    /// </summary>
-//    public static class KnownFolders
-//    {
-//    }
-//}
+namespace Windows.Storage
+{
+    /// <summary>
+    /// Provides access to common locations that contain user content. This includes content from a user's local libraries (such as Documents, Pictures, Music, and Videos), HomeGroup, removable devices, and media server devices.
+    /// </summary>
+    public static class KnownFolders
+    {
+        //public static StorageFolder AppCaptures { get; }
+        //public static StorageFolder CameraRoll { get; }
+        //public static StorageFolder DocumentsLibrary { get; }
+        //public static StorageFolder HomeGroup { get; }
+        //public static StorageFolder MediaServerDevices { get; }
+        //public static StorageFolder MusicLibrary { get; }
+        //public static StorageFolder Objects3D { get; }
+        //public static StorageFolder PicturesLibrary { get; }
+        //public static StorageFolder Playlists { get; }
+        //public static StorageFolder RecordedCalls { get; }
+
+        /// <summary>
+        /// Gets the removable devices folder.
+        /// </summary>
+        public static StorageFolder RemovableDevices { get; }
+
+        //public static StorageFolder SavedPictures { get; }
+        //public static StorageFolder VideosLibrary { get; }
+
+        //public static IAsyncOperation<StorageFolder> GetFolderForUserAsync(User user, KnownFolderId folderId)
+        //{
+
+        //}
+    }
+}
