5c25abcea88c6210c88909f6d91dfc66177ce331
José Simões <jose.simoes@eclo.solutions>
2018-11-16 16:43:44 +0000

    Add test app
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

---
+++ source/AccessSDcard/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using Windows.Storage;
+
+namespace AccessSDcard
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Get the logical root folder for all removable storage devices
+            StorageFolder externalDevices = Windows.Storage.KnownFolders.RemovableDevices;
+
+            // list all removable storage devices
+            var removableDevices = externalDevices.GetFolders();
+
+            // get folders on SDCard (which should be the 1st device)
+            var foldersInSDcard = removableDevices[0].GetFolders();
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
